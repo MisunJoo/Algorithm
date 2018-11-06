727dd2e491d377641e5bc558e279557bc8587ff0
MisunJoo <0987someday@naver.com>
2018-11-06 21:57:18 +0900

    알고리즘 - 이진검색트리 Q1

--- src/main/java/DoIt/List/LinkedList.java
+++ src/main/java/DoIt/List/LinkedList.java
@@ -1,4 +1,13 @@
 package DoIt.List;
 
 public class LinkedList {
+    class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+    }
 }
--- src/main/java/DoIt/Tree/BinTree.java
+++ src/main/java/DoIt/Tree/BinTree.java
@@ -1,4 +1,40 @@
 package DoIt.Tree;
 
-public class BinTree {
+import java.util.Comparator;
+
+public class BinTree<K,V> {
+    static class Node<K,V>{
+        private K key;
+        private V data;
+        private Node<K,V> left;
+        private Node<K,V> right;
+
+        Node(K key, V data, Node<K,V> left, Node<K,V> right){
+            this.key = key;
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        K getKey(){
+            return key;
+        }
+
+        V getValue(){
+            return data;
+        }
+
+        void print(){
+            System.out.println(data);
+        }
+    }
+
+    private Node<K,V> root;
+    private Comparator<? super K> comparator = null;
 }
+
+
+
+
+
+
