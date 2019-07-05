e029dd5b67279bf204c6859a7c17c18ede1667a5
MisunJoo <0987someday@naver.com>
2019-07-05 22:17:53 +0900

    [ADD] 상하좌우탐색

--- src/main/java/More/baekjoon/BFS/test.java
+++ src/main/java/More/baekjoon/BFS/test.java
@@ -3,8 +3,8 @@ package More.baekjoon.BFS;
 public class test {
 
   public static void main(String[] args) {
-    System.out.println(Integer.toString(4/7));
-    System.out.println(Integer.toString(4%7));
+    System.out.println(Integer.toString(4 / 7));
+    System.out.println(Integer.toString(4 % 7));
 
     for (int location = 0; location < 49; location++) {
       int currentX = location / 7; // 0000000 1111111 222222
@@ -14,6 +14,30 @@ public class test {
       System.out.print(Integer.toString(currentY));
     }
 
+    /* 오, 왼, 상, 하를 다 탐색할 수 있음 */
+    /* (2,2)의 경우 (1,2), (3,2), (2,3), (2,1) */
+    int[][] moves = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+    for (int[] move : moves) {
+      System.out.println(move[0]);
+      System.out.println(move[1]);
+
+      current current = new current(2, 2);
+
+      int nextX = current.x + move[0];
+      int nextY = current.y + move[1];
+    }
   }
+
+  public static class current {
+    public int x;
+    public int y;
+
+    public current(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+
 
 }
